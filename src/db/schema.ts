6027e834f09b3063f72de251470c3ff840ad1937
Ojas Singh <ojas.singh02@gmail.com>
2025-07-29 12:30:22 -0400

    Update DB schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,10 +1,29 @@
-import { date, integer, pgTable, varchar } from "drizzle-orm/pg-core";
-export const patientsTable = pgTable("patients", {
+import {
+  date,
+  integer,
+  pgEnum,
+  pgTable,
+  varchar,
+  jsonb,
+} from "drizzle-orm/pg-core";
+
+export const PatientStatus = pgEnum("patient_status", [
+  "Inquiry",
+  "Onboarding",
+  "Active",
+  "Churned",
+]);
+
+export const patients = pgTable("patients", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   firstName: varchar({ length: 255 }).notNull(),
   middleName: varchar({ length: 255 }).notNull(),
   lastName: varchar({ length: 255 }).notNull(),
   dateOfBirth: date().notNull(),
-  address: varchar({ length: 255 }).notNull(),
-  status: varchar({ length: 255 }).notNull(),
+  street: varchar({ length: 255 }).notNull(),
+  city: varchar({ length: 100 }).notNull(),
+  state: varchar({ length: 50 }).notNull(),
+  zip: varchar({ length: 20 }).notNull(),
+  status: PatientStatus(),
+  conditions: jsonb().notNull().default("[]"),
 });
